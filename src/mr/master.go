ed165eabe012fe0307fbb4b22a67a80c34d1a2f1
陈嘉俊 <358035351@qq.com>
2021-05-14 00:21:01 -0700

    mapreduce

--- src/mr/master.go
+++ src/mr/master.go
@@ -1,29 +1,159 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"context"
+	"errors"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
 
+// worker状态
+const (
+	TaskIdle    = iota // 闲置
+	TaskWorking        // 正在运行
+	TaskCommit         // 完成
+)
 
 type Master struct {
 	// Your definitions here.
+	files   []string
+	nReduce int
 
+	mapTasks    []int
+	reduceTasks []int
+
+	mapCount     int
+	workerCommit map[string]int // worker对应的状态
+	allCommited  bool
+
+	timeout time.Duration
+	mu      sync.Mutex
 }
 
 // Your code here -- RPC handlers for the worker to call.
+func (m *Master) Work(args *WorkArgs, reply *WorkReply) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	for k, v := range m.files {
+		if m.mapTasks[k] != TaskIdle {
+			continue
+		}
+		reply.FileName = v
+		reply.TaskId = k
+		reply.MapReduce = "map"
+		reply.IsFinished = false
+		reply.FileNumber = m.nReduce
+		m.workerCommit[args.WorkerId] = TaskWorking
+		m.mapTasks[k] = TaskWorking
+		ctx, _ := context.WithTimeout(context.Background(), m.timeout)
+		go func() {
+			select {
+			case <-ctx.Done():
+				{
+
+					m.mu.Lock()
+					defer m.mu.Unlock()
+					if m.workerCommit[args.WorkerId] != TaskCommit && m.mapTasks[k] != TaskCommit {
+						m.mapTasks[k] = TaskIdle
+					}
+				}
+			}
+		}()
+		return nil
+	}
+
+	for k, v := range m.reduceTasks {
+		//map is not finished
+		if m.mapCount != len(m.files) {
+			return nil
+		}
+		if v != TaskIdle {
+			continue
+		}
+		reply.TaskId = k
+		reply.MapReduce = "reduce"
+		reply.FileNumber = len(m.files)
+		reply.IsFinished = false
+		reply.FileName = ""
+		m.workerCommit[args.WorkerId] = TaskWorking
+		m.reduceTasks[k] = TaskWorking
+		ctx, _ := context.WithTimeout(context.Background(), m.timeout)
+		go func() {
+			select {
+			case <-ctx.Done():
+				{
+
+					m.mu.Lock()
+					defer m.mu.Unlock()
+					if m.workerCommit[args.WorkerId] != TaskCommit && m.reduceTasks[k] != TaskCommit {
+						m.mapTasks[k] = TaskIdle
+
+					}
+				}
+			}
+		}()
+		return nil
+	}
+
+	for _, v := range m.workerCommit {
+		if v == TaskWorking {
+			reply.IsFinished = false
+			return nil
+		}
+	}
+	reply.IsFinished = true
+	return errors.New("worker apply but no tasks to dispatch")
+
+}
+
+func (m *Master) Commit(args *CommitArgs, reply *CommitReply) error {
+	//log.Println("a worker", args.WorkerId, "commit a", args.MapReduce, "task:", args.TaskId)
+	m.mu.Lock()
+	switch args.MapReduce {
+	case "map":
+		{
+			m.workerCommit[args.WorkerId] = TaskCommit
+
+			m.mapTasks[args.TaskId] = TaskCommit
+			m.mapCount++
+		}
+
+	case "reduce":
+		{
+			m.reduceTasks[args.TaskId] = TaskCommit
+			m.workerCommit[args.WorkerId] = TaskCommit
+		}
+
+	}
+	m.mu.Unlock()
+	for _, v := range m.mapTasks {
+		if v != TaskCommit {
+			return nil
+		}
+	}
+	for _, v := range m.reduceTasks {
+		if v != TaskCommit {
+			return nil
+		}
+	}
+	m.allCommited = true
+	return nil
+}
 
 //
-// an example RPC handler.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
+//an example RPC handler.
+
+//the RPC argument and reply types are defined in rpc.go.
+
 func (m *Master) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
-
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -39,6 +169,7 @@ func (m *Master) server() {
 		log.Fatal("listen error:", e)
 	}
 	go http.Serve(l, nil)
+
 }
 
 //
@@ -46,25 +177,32 @@ func (m *Master) server() {
 // if the entire job has finished.
 //
 func (m *Master) Done() bool {
-	ret := false
-
-	// Your code here.
-
-
-	return ret
+	return m.allCommited
 }
 
-//
-// create a Master.
-// main/mrmaster.go calls this function.
-// nReduce is the number of reduce tasks to use.
-//
+
+//create a Master.
+//main/mrmaster.go calls this function.
+//nReduce is the number of reduce tasks to use.
+
 func MakeMaster(files []string, nReduce int) *Master {
-	m := Master{}
+	m := Master{
+		files:        files,
+		nReduce:      nReduce,
+		mapTasks:     make([]int, len(files)),
+		reduceTasks:  make([]int, nReduce),
+		workerCommit: make(map[string]int),
+		allCommited:  false,
+		timeout:      10 * time.Second,
+	}
 
-	// Your code here.
-
-
+	//log.Println("[init] with:", files, nReduce)
 	m.server()
 	return &m
 }
+
+
+
+
+
+
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,9 +1,60 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"crypto/rand"
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+)
+
+
+//
+// main/mrworker.go calls this function.
+// 启动无限循环，不断向master发出请求
+func Worker(mapf func(string, string) []KeyValue,
+	reducef func(string, []string) string) {
+
+	workId := genWorkerID()
+	retry := 3
+	for {
+		workargs := WorkArgs{WorkerId: workId}
+		workreply := WorkReply{}
+		working := call("Master.Work", &workargs, &workreply)
+		if workreply.IsFinished || !working {
+			return
+		}
+		switch workreply.MapReduce {
+		case "map":
+			MapWork(workreply, mapf)
+			retry = 3
+		case "reduce":
+			ReduceWork(workreply, reducef)
+			retry = 3
+		default:
+			if retry < 0 {
+				return
+			}
+			retry--
+		}
+		commitargs := CommitArgs{
+			WorkerId:  workId,
+			TaskId:    workreply.TaskId,
+			MapReduce: workreply.MapReduce,
+		}
+		commitReply := CommitReply{}
+		_ = call("Master.Commit", &commitargs, &commitReply)
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+
 
 
 //
@@ -13,6 +64,14 @@ type KeyValue struct {
 	Key   string
 	Value string
 }
+
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 //
 // use ihash(key) % NReduce to choose the reduce
@@ -24,18 +83,91 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+func genWorkerID() (uuid string) {
+	unix32bits := uint32(time.Now().UTC().Unix())
+	buff := make([]byte, 12)
+	numRead, err := rand.Read(buff)
+	if numRead != len(buff) || err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("%x-%x-%x-%x-%x-%x\n", unix32bits, buff[0:2], buff[2:4], buff[4:6], buff[6:8], buff[8:])
+}
 
-//
-// main/mrworker.go calls this function.
-//
-func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
 
-	// Your worker implementation here.
 
-	// uncomment to send the Example RPC to the master.
-	// CallExample()
+func MapWork(task WorkReply, mapf func(string, string) []KeyValue) {
+	file, err := os.Open(task.FileName)
+	if err != nil {
+		log.Fatalf("cannot open %v", task.FileName)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", task.FileName)
+	}
+	file.Close()
+	kva := mapf(task.FileName, string(content))
+	sort.Sort(ByKey(kva))
 
+	tmpName := "mr-tmp-" + strconv.Itoa(task.TaskId)
+	var fileNum = make(map[int]*json.Encoder)
+	for i := 0; i < task.FileNumber; i++ {
+		ofile, _ := os.Create(tmpName + "-" + strconv.Itoa(i))
+		fileNum[i] = json.NewEncoder(ofile)
+		defer ofile.Close()
+	}
+	for _, kv := range kva {
+		key := kv.Key
+		reduceIdx := ihash(key) % task.FileNumber
+		err := fileNum[reduceIdx].Encode(&kv)
+		if err != nil {
+			log.Fatal("Unable to write to file")
+		}
+	}
+}
+
+func ReduceWork(task WorkReply, reducef func(string, []string) string) {
+	intermediate := []KeyValue{}
+
+	for mapTaskNumber := 0; mapTaskNumber < task.FileNumber; mapTaskNumber++ {
+		filename := "mr-tmp-" + strconv.Itoa(mapTaskNumber) + "-" + strconv.Itoa(task.TaskId)
+		f, err := os.Open(filename)
+		if err != nil {
+			log.Fatal("Unable to read from:", filename)
+		}
+		defer f.Close()
+		decoder := json.NewDecoder(f)
+		var kv KeyValue
+		for decoder.More() {
+			err := decoder.Decode(&kv)
+			if err != nil {
+				log.Fatal("Json decode failed,", err)
+			}
+			intermediate = append(intermediate, kv)
+		}
+	}
+	sort.Sort(ByKey(intermediate))
+
+	i := 0
+	ofile, err := os.Create("mr-out-" + strconv.Itoa(task.TaskId+1))
+	if err != nil {
+		log.Fatal("Unable to create file:", ofile)
+	}
+	defer ofile.Close()
+	//log.Println("complete to", task.TaskId, "start to write in to", ofile.Name())
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+		fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
+		i = j
+	}
+	ofile.Close()
 }
 
 //
